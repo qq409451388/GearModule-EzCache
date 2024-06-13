6203b32924136b0266606c05b712376df9ed6e96
guohan <poethan@foxmail.com>
2024-06-13 16:10:20 +0800

    feat: the object alive logic

--- EzCache.php
+++ EzCache.php
@@ -2,11 +2,13 @@
 abstract class EzCache implements IEzCacheSystem,IEzCacheKey,IEzCacheString,IEzCacheList,IEzCacheHash
 {
     public static function getInstance():EzCache {
-        if (null === static::$ins) {
+        return new static();
+        /*if (null === static::$ins) {
             static::$ins = new static();
         }
-        return static::$ins;
+        return static::$ins;*/
     }
+
     public function connect($ip, $port, $timeout) {
 
     }
